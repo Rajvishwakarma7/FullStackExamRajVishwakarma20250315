84dd8d5093e464664bd5cba1a63d1ef5dc8af110
Raj <your-raj7280021@gamil.com>
2025-03-16 01:48:52 +0530

    new changes

--- client/next.config.mjs
+++ client/next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  output: "export",
+  output: "export", // ✅ Required for Netlify (Replaces next export)
 };
 
 export default nextConfig;
