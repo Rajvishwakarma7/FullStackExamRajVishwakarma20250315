14b331d1864e121cca1aa0184f94ee7001f14f9b
Raj <your-raj7280021@gamil.com>
2025-03-15 22:55:37 +0530

    new changes related env

--- server/src/config/db.js
+++ server/src/config/db.js
@@ -7,6 +7,7 @@ const mysqlPool = mysql.createPool({
   user: process.env.MYSQL_USER,
   password: process.env.MYSQL_PASSWORD,
   database: process.env.MYSQL_DB,
+  port: process.env.MYSQL_PORT,
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
